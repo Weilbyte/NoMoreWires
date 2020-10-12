4896ff8d2bd03ecbe61510b2de54acb7fbe2da33
Weilbyte <git@weilbyte.dev>
2020-10-12 04:16:50 +0200

    0.1.0

---
+++ src/NoPowerWires.cs
@@ -0,0 +1,22 @@
+﻿namespace NoPowerWires
+{
+    using Verse;
+    using HarmonyLib;
+    using System.Reflection;
+
+    public class Mod : Verse.Mod
+    {
+        public Mod(ModContentPack content) : base(content) {
+            Harmony noPowerWires = new Harmony("weilbyte.rimworld.nopowerwires");
+
+            MethodInfo targetMethod = AccessTools.Method(typeof(RimWorld.PowerNetGraphics), "PrintWirePieceConnecting");
+            HarmonyMethod prefixMethod = new HarmonyMethod(AccessTools.Method(typeof(NoPowerWires.Mod), "PrintWirePieceConnecting_Prefix"));
+
+            noPowerWires.Patch(targetMethod, prefixMethod);
+        }
+
+        public static bool PrintWirePieceConnecting_Prefix(bool forPowerOverlay) {
+            return forPowerOverlay;
+        }
+    }
+}
